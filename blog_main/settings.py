c4174bf0d1606338c62f43fc33545f0813881a72
Harsh Raj <harshraj.2222006@gmail.com>
2026-06-28 22:23:43 +0530

    Update ALLOWED_HOSTS for deployment

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -15,7 +15,11 @@ SECRET_KEY = 'django-insecure-n8^@1$ll9wyo)$h5h+5xq-a7a!vnn&@5(gi#!3*9#t@=nqc&al
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "blogging-system-sahj.onrender.com",
+]
 
 
 # Application definition
