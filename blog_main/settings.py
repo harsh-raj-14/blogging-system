b08d02003fd2e803d8556d8852a4befd550efb24
Harsh Raj <harshraj.2222006@gmail.com>
2026-07-01 15:55:34 +0530

    Update blogging system

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -32,6 +32,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://blogging-system-sahj.onrender.com",
 ] 
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
